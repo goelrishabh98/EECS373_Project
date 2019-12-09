7e106de8712dcf9549354937f00a45847bf967df
Bansal <bansarth@UMICH.EDU>
2019-12-09 17:32:51 -0500

    got circles!

--- WhiteBoard_Plotter/SoftConsole/WhiteBoard_Plotter_MSS_CM3_0/WhiteBoard_Plotter_MSS_CM3_0_app/main.c
+++ WhiteBoard_Plotter/SoftConsole/WhiteBoard_Plotter_MSS_CM3_0/WhiteBoard_Plotter_MSS_CM3_0_app/main.c
@@ -5,26 +5,46 @@
 //Current Dimensions: 57, 85
 //center: 42.5, 28.5
 int main(){
+	//uint8_t servoExtend[1] = "e";
+	//uint8_t servoRetract[1] = "r";
+	//MSS_UART_init(&g_mss_uart1, MSS_UART_57600_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+	//MSS_UART_set_rx_handler( &g_mss_uart1, uart1_rx_handler, MSS_UART_FIFO_SINGLE_BYTE);
 	stepper_config(SIXTEENTH, SIXTEENTH);
 	interfaceConfig(57, 85);
 
-	makeLine(-5, 5);
-	makeLine(15, 0.0);
-	returnToCenter();
+	makeLine(-0.0576400000000007,-0.58527);
+	makeLine(-0.17072,-0.56278);
+	makeLine(-0.277229999999999,-0.51866);
+	makeLine(-0.373090000000001,-0.45461);
+	makeLine(-0.45461,-0.373090000000001);
+	makeLine(-0.518660000000001,-0.277229999999999);
+	makeLine(-0.56278,-0.17072);
+	makeLine(-0.58527,-0.0576400000000007);
+	makeLine(-0.58527,0.0576400000000007);
+	makeLine(-0.56278,0.17072);
+	makeLine(-0.51866,0.277229999999999);
+	makeLine(-0.45461,0.373090000000001);
+	makeLine(-0.37309,0.45461);
+	makeLine(-0.27723,0.51866);
+	makeLine(-0.17072,0.56278);
+	makeLine(-0.05764,0.58527);
+	makeLine(0.05764,0.58527);
+	makeLine(0.17072,0.56278);
+	makeLine(0.27723,0.51866);
+	makeLine(0.37309,0.45461);
+	makeLine(0.45461,0.37309);
+	makeLine(0.51866,0.27723);
+	makeLine(0.56278,0.17072);
+	makeLine(0.58527,0.05764);
+	makeLine(0.58527,-0.05764);
+	makeLine(0.56278,-0.17072);
+	makeLine(0.518660000000001,-0.27723);
+	makeLine(0.45461,-0.37309);
+	makeLine(0.373090000000001,-0.45461);
+	makeLine(0.277229999999999,-0.51866);
+	makeLine(0.17072,-0.56278);
+	makeLine(0.0576400000000007,-0.58527);
 
-	/*uint8_t servoExtend[1] = "e";
-	uint8_t servoRetract[1] = "r";
-	MSS_UART_init(&g_mss_uart1, MSS_UART_57600_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
-	MSS_UART_set_rx_handler( &g_mss_uart1, uart1_rx_handler, MSS_UART_FIFO_SINGLE_BYTE);
-
-
-	int a = 1;
-	while(1) {
-		sendMessage(&servoRetract, 1, 0x373A);
-		a = 1;
-		sendMessage(&servoExtend, 1, 0x373A);
-		a = 1;
-	}*/
 	/*
 	makeLine(2.0, -0.7);
 	sendMessage(servoExtend, 1, 0x373A);
